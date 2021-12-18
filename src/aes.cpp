dc60ce6e307609d383ed99fa6dc848fce7a76bf8
Larry Skuse <lskuse3@gatech.edu>
2021-12-18 12:33:07 -0600

    Create code structure

---
+++ src/aes.cpp
@@ -0,0 +1,86 @@
+#include "lskuse/aes.h"
+#include "aesblock.h"
+#include "KeySchedule.h"
+
+using namespace lskuse;
+
+/*************************************************************************************************/
+AES::AES(Mode mode, KeyLen keyLen, Padding padding) :
+  m_mode(mode), 
+  m_keyLen(keyLen), 
+  m_padding(padding)
+{
+}
+
+/*************************************************************************************************/
+std::string AES::encrypt(const std::string& plaintext, const std::string& key, 
+                         Mode mode, KeyLen keyLen, Padding padding)
+{
+  AES aes(mode, keyLen, padding);
+  return aes.encrypt(plaintext, key);
+}
+
+/*************************************************************************************************/
+std::string AES::decrypt(const std::string& ciphertext, const std::string& key, 
+                         Mode mode, KeyLen keyLen, Padding padding)
+{
+  AES aes(mode, keyLen, padding);
+  return aes.decrypt(ciphertext, key);
+}
+
+/*************************************************************************************************/
+std::string AES::encrypt(const std::string& plaintext, const std::string& key)
+{
+  std::string ciphertext;
+
+  KeySchedule keySchedule(m_keyLen, key);
+  switch(m_mode)
+  {
+    case Mode::ECB:
+    {
+      // ECB encrypts blocks separately
+      for(unsigned i = 0; i < plaintext.size(); i += AESBlock::sizeInBytes())
+      {
+        AESBlock block(m_padding, plaintext.substr(i, AESBlock::sizeInBytes()), keySchedule);
+        ciphertext += block.encrypt();
+      }
+      break;
+    }
+    case Mode::CBC:
+    {
+      // TODO: implement CBC encryption
+      break;
+    }
+  }
+
+  return ciphertext;
+}
+
+/*************************************************************************************************/
+std::string AES::decrypt(const std::string& ciphertext, const std::string& key)
+{
+  std::string plaintext;
+
+  KeySchedule keySchedule(m_keyLen, key);
+  switch(m_mode)
+  {
+    case Mode::ECB:
+    {
+      // ECB decrypts blocks separately
+      for(unsigned i = 0; i < ciphertext.size(); i += AESBlock::sizeInBytes())
+      {
+        AESBlock block(m_padding, ciphertext.substr(i, AESBlock::sizeInBytes()), keySchedule);
+        plaintext += block.decrypt();
+      }
+      break;
+    }
+    case Mode::CBC:
+    {
+      // TODO: implement CBC decryption
+      break;
+    }
+  }
+
+  return plaintext;
+}
+
