42f2017f88b1cb4c3fef218b56de5f6f5f28a32c
Larry Skuse <lskuse3@gatech.edu>
2021-12-19 12:27:41 -0600

    Implement 128, 192, and 256 bit key schedules

--- src/aeskeysched.h
+++ src/aeskeysched.h
@@ -11,21 +11,39 @@ namespace lskuse
   {
     public:
       AESKeySchedule(AES::KeyLen keyLen, const std::string& key);
+      ~AESKeySchedule();
 
-      char* getRoundKey(unsigned round);
+      inline bool isValid() const {return m_valid;}
+      char* getRoundKey(unsigned round) const;
 
     private:
       void computeKeySchedule();
-      void rotWord();
-      void subWord();
-      void rcon();
+      void compute128KeySchedule();
+      void compute192KeySchedule();
+      void compute256KeySchedule();
+      void rotWord(char* word);
+      void subWord(char* word);
+      void rcon(char* word);
+
+      static constexpr const unsigned WORD_LEN = 4;
+      static constexpr const unsigned ROUND_KEY_LEN = 16;
+      static constexpr const unsigned LEN_128_IN_BYTES = 16;
+      static constexpr const unsigned LEN_192_IN_BYTES = 24;
+      static constexpr const unsigned LEN_256_IN_BYTES = 32;
+      static constexpr const unsigned LEN_128_WORD_ARRAY_SIZE = 44 * WORD_LEN;
+      static constexpr const unsigned LEN_192_WORD_ARRAY_SIZE = 54 * WORD_LEN; // really only needs to be 52
+      static constexpr const unsigned LEN_256_WORD_ARRAY_SIZE = 64 * WORD_LEN; // really only needs to be 60
+      static constexpr const unsigned LEN_128_TRANSFORMATIONS = LEN_128_WORD_ARRAY_SIZE / LEN_128_IN_BYTES;
+      static constexpr const unsigned LEN_192_TRANSFORMATIONS = LEN_192_WORD_ARRAY_SIZE / LEN_192_IN_BYTES;
+      static constexpr const unsigned LEN_256_TRANSFORMATIONS = LEN_256_WORD_ARRAY_SIZE / LEN_256_IN_BYTES;
 
       AES::KeyLen        m_keyLen;
       std::string        m_key;
       bool               m_isEncrypt;
+      bool               m_valid;
       std::vector<char*> m_keySchedule;
 
-      static constexpr const uint8_t RCON[11] = {0x8d, 0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36};
+      static constexpr const uint8_t RCON[10] = {0x01, 0x02, 0x04, 0x08, 0x10, 0x20, 0x40, 0x80, 0x1b, 0x36};
   };
 }
 
